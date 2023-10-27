a104ee7040ab46829a945c6adc1afc601654bc17
porya84 <76752078+porya84@users.noreply.github.com>
2023-10-27 09:54:35 +0330

    Build next step func

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ let statusIconsElem = $.querySelectorAll(".status_icon");
 let nextStepElem = $.getElementById("next_step");
 let resetBtn = $.getElementById("reset");
 let stepLocatin = -1;
+
+
+// Go to the next steps 
+nextStepElem.addEventListener("click", () => {
+    if (stepLocatin != 3) {
+      stepLocatin++;
+  
+      stepsElem[stepLocatin].classList.remove("current_step");
+      stepsElem[stepLocatin].classList.add("checked_step");
+  
+      statusIconsElem[stepLocatin].classList.remove("current_step_icon");
+      statusIconsElem[stepLocatin].classList.add("check_icon");
+  
+      if (stepLocatin !== 3) {
+        progressLineEelms[stepLocatin].classList.remove("Unchecked_line");
+        progressLineEelms[stepLocatin].classList.add("checked_line");
+  
+        stepsElem[stepLocatin + 1].classList.remove("next_step");
+        stepsElem[stepLocatin + 1].classList.add("current_step");
+  
+        statusIconsElem[stepLocatin + 1].classList.remove("next_step_icon");
+        statusIconsElem[stepLocatin + 1].classList.add("current_step_icon");
+  
+      }
+    }
+  });
