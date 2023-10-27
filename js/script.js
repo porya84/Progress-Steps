a7d1fa865f4d646e11d8ee8dbc6da5dd41cc7bb6
porya84 <76752078+porya84@users.noreply.github.com>
2023-10-27 09:53:13 +0330

    set Variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let $ = document;
+let stepsElem = $.querySelectorAll(".step_body");
+let progressLineEelms = $.querySelectorAll(".progress_line_body");
+let statusIconsElem = $.querySelectorAll(".status_icon");
+let nextStepElem = $.getElementById("next_step");
+let resetBtn = $.getElementById("reset");
+let stepLocatin = -1;
